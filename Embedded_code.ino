8d78dbc96d6fa3befe466299fb211e32e5e447c0
ibrahimrizq87 <ibrahimrizqabd@gmail.com>
2023-11-19 21:33:01 +0200

    last commit

--- Embedded_code.ino
+++ Embedded_code.ino
@@ -1,35 +1,53 @@
 
+/* 
+ * File:   Embedded Graduation project
+ * Author: Ibrhime Rizq
+ * description: the main purpose of this code is to connect the esp to the firebase project.
+ *              start generating a QRcode on the screen only if the state on the firebase is true ,
+ *              which means that the lecture or the section on the hall has been given the permission to generat the qrcode 
+ *              which then used to record the attendance by scanning the code  
+ * Created on MAR 3, 2023, 9:53 PM
+ */
+ 
+/* -------------------------------Section : Includes--------------------------------*/
 
+/* including the esp8266WiFi library*/
 #include <ESP8266WiFi.h>
+
+/* including the firbase library*/
 #include <FirebaseESP8266.h>
 
+/* including the Adafruit library for the screen*/
+#include <Adafruit_GFX.h>      
 
+/* including the Adafruit_ST7735 library for screen module functions*/
+#include <Adafruit_ST7735.h>   
+
+/* including the SPI library for Serial Peripheral Interface communication protocol*/
+#include <SPI.h>
+
+/* including the QRcode library to generate QRcode for st7735 module*/
+#include <qrcode_st7735.h>
+
+/* ------------------------------- Section: Macro Declarations --------------------------------*/
 #define TFT_RST   16  
 #define TFT_CS    4    
 #define TFT_DC    5 
 
-#include <Adafruit_GFX.h>      
-#include <Adafruit_ST7735.h>   
-#include <SPI.h>
-#include <qrcode_st7735.h>
-
 #define TFTMODEL INITR_144GREENTAB
-
-//#include <addons/TokenHelper.h>
-
-//#include <addons/RTDBHelper.h>
-
+/* Declarations of network username and password */
 #define WIFI_SSID "BEMO RIZQ"
 #define WIFI_PASSWORD "123456789"
 
-
+/* Declarations of firbase API key*/
 #define API_KEY "T4u6rID3GJY1hL2ASYnC84QdT1jQv7Cz4kyqE431"
-
+/* Declarations of firbase database URL*/
 #define DATABASE_URL "https://graduation-ef946-default-rtdb.firebaseio.com/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
 const String code="ABC123";
 const String hallID="M44";
-FirebaseData fbdo;
 
+FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
@@ -41,16 +59,17 @@ String main2="";
 
 int a, b, x, y;
 bool state=false;
-
 void setup()
 {
 
   Serial.begin(115200);
-delay(2000);
+  delay(2000);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
+  /* Initialte the connection between the esp and the network source*/
   while (WiFi.status() != WL_CONNECTED)
   {
+    /*Whilte until the connection happens*/
     Serial.print(".");
     delay(300);
   }
@@ -62,20 +81,15 @@ delay(2000);
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
   config.api_key = API_KEY;
-
   config.database_url = DATABASE_URL;
 
 
 
-
+/*configers the connection with firebase*/
   Firebase.begin(DATABASE_URL, API_KEY);
-
-
-
   Firebase.setDoubleDigits(5);
-
     display.initR(TFTMODEL);
-    // Initialize QRcode display using library
+
     qrcode.init();
     qrcode.create("hellow world!");
 
@@ -84,46 +98,33 @@ delay(2000);
 void loop()
 {
  
-  
+  /* generat a rondom code which will be used to generat the qr code and to be sent to the 
+   * firbase for the application to check if the code scanned from the QRcode and the code in the firebase is the same
+   */
     x=random(0,1000);
-    //y=random(10,19);
-    
-   
+
+   /*if the firebase connection is good*/
   if (Firebase.ready()) 
   {
+    /*read the state from the firebase database*/
     Serial.printf("Get boolean state data:  %s\n", Firebase.getBool(fbdo, "/hall/"+hallID+"/state") ? String(fbdo.to<bool>()).c_str() : fbdo.errorReason().c_str());
     state=fbdo.to<bool>();
     if (state){ 
+      
        qrcode.init();
-    qrcode.create(hallID+"@"+code+"@"+x);
-      Firebase.setInt(fbdo, "/embedded/"+hallID+"/code", x);
-    Firebase.setString(fbdo, "/embedded/"+hallID+"/hallID", hallID);
-  
-      Serial.println("value is true ");
+       /*generate a qrcode using the random code generated*/
+       qrcode.create(hallID+"@"+code+"@"+x);
+       /*send the code to firebase*/
+       Firebase.setInt(fbdo, "/embedded/"+hallID+"/code", x);
+       Firebase.setString(fbdo, "/embedded/"+hallID+"/hallID", hallID);
+       Serial.println("value is true ");
        Serial.println("Code is : ");Serial.print(x);
       }else{
+        
         qrcode.init();
         Serial.println("value is false ");
       }
-    //Firebase.setInt(fbdo, main, 5);
-  
-
-    /*Serial.printf("Get int a--  %s\n", Firebase.getInt(fbdo, "/test/a") ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
-     a=fbdo.to<int>();
-    Serial.printf("Get int b--  %s\n", Firebase.getInt(fbdo, "/test/b") ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
-     b=fbdo.to<int>();
-
-  Serial.println();  
-  Serial.print("a:");
-  Serial.print(a);
-  Serial.print("  b: ");
-  Serial.print(b);
-  
-  Serial.println();
-  Serial.println("------------------");
-  Serial.println();
-  
-*/
+  /*repeat the code every 7 seconds*/
   delay(7000);
   }
 }
